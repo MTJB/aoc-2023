c6e12b50ded37a8ad8384d676b158ec7e40337c4
MTJB <markbrown04@googlemail.com>
2023-12-02 11:47:25 +0000

    Day 2 pt.2

--- aoc_2023/day_02.py
+++ aoc_2023/day_02.py
@@ -1,10 +1,11 @@
 import re
+from functools import reduce
 
 
 def main():
     file_path = '../resources/day_02.txt'
-    # Part 1
     print(f'Part 1 Answer: {sum_valid_games(file_path)}')
+    print(f'Part 2 Answer: {sum_powers(file_path)}')
 
 
 def sum_valid_games(file_path) -> int:
@@ -13,12 +14,12 @@ def sum_valid_games(file_path) -> int:
 
     with open(file_path, 'r', encoding='utf-8-sig') as file:
         for line in file:
-            valid_game_ids += process_game_line(line, limits)
+            valid_game_ids += get_valid_game_id(line, limits)
 
     return valid_game_ids
 
 
-def process_game_line(game_string, limits):
+def get_valid_game_id(game_string, limits):
     game_id_match = re.search(r'Game (\d+):', game_string)
     if not game_id_match:
         return 0
@@ -32,6 +33,32 @@ def process_game_line(game_string, limits):
                 return 0
 
     return game_id
+
+
+def sum_powers(file_path) -> int:
+    sum_of_powers = 0
+
+    with open(file_path, 'r', encoding='utf-8-sig') as file:
+        for line in file:
+            sum_of_powers += find_power(line)
+
+    return sum_of_powers
+
+
+def find_power(game_string):
+    game_id_match = re.search(r'Game (\d+):', game_string)
+    if not game_id_match:
+        return 0
+
+    game_data = game_string.replace(game_id_match.group(0), '')
+
+    max_values = {"red": 0, "green": 0, "blue": 0}
+    for game_set in game_data.split(';'):
+        for number, color in re.findall(r'(\d+) (\w+)', game_set):
+            if int(number) > max_values.get(color, float('inf')):
+                max_values[color] = int(number)
+
+    return reduce(lambda x, y: x * y, max_values.values())
 
 
 if __name__ == "__main__":
